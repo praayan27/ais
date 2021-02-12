8bfadd29692c7322e11d1c4e2f7ce09b59fb5f29
srikanth <srko831@ess.ais.ac.nz>
2021-02-12 18:11:40 +1300

    commit

--- test.java
+++ test.java
@@ -4,7 +4,7 @@ public static void main(String[] args)
 {
 int tableNo = 0;
 for (int i = 0 ; i<5; i++)
-System.println(tableNo + "*" + i + " = " + tableNo *i);
+System.out.println(tableNo + "*" + i + " = " + tableNo *i);
 
 }
 
